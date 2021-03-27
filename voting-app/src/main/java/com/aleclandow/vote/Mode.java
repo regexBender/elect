b2ff0af8f1a425d0f4901e49227355340d5a5714
Alec Landow <alec.landow@cengage.com>
2021-03-27 19:59:20 -0400

    Add modes

---
+++ voting-app/src/main/java/com/aleclandow/vote/Mode.java
@@ -0,0 +1,28 @@
+package com.aleclandow.vote;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum Mode {
+
+    REGISTER("register"),
+    VOTE("vote"),
+    ADMIN("admin"),
+    EXIT("exit");
+
+    private final String mode;
+
+    public static String ALL_MODES = String.join(" | ", getAllModesList());
+
+    public String toString() {
+        return mode;
+    }
+
+    private static List<String> getAllModesList() {
+        return Arrays.stream(Mode.values()).map(Mode::toString).collect(Collectors.toList());
+    }
+
+}
--- voting-app/src/main/java/com/aleclandow/vote/VotingApp.java
+++ voting-app/src/main/java/com/aleclandow/vote/VotingApp.java
@@ -4,16 +4,39 @@ import com.aleclandow.util.ConsoleColors;
 import java.util.Scanner;
 
 public class VotingApp {
+
     public static void main(String[] args) {
-        System.out.print(ConsoleColors.BLUE);
-        System.out.println("*** Welcome to Alec's Blockchain-Based Voting Program ***");
+        System.out.print(ConsoleColors.CYAN);
+        System.out.println("* Welcome to the Secure Voting Program using a Permissioned BlockChain *");
+        System.out.println("- Using IBM's HyperLedger Fabric");
+        System.out.println("Author: Alec Landow");
+        System.out.println("For: CSC 724 at NCSU, Spring 2021");
         System.out.print(ConsoleColors.RESET);
 
         String voterId;
         Scanner input = new Scanner(System.in);
-        System.out.println(ConsoleColors.RED);
-        System.out.println("To register, please enter your voter ID and password:");
-        voterId = input.nextLine();
+
+        while(true) {
+            System.out.println(ConsoleColors.BLUE);
+            System.out.println("Options: " + Mode.ALL_MODES);
+            String mode = input.nextLine();
+
+            if (mode.matches("(?i)" + Mode.ADMIN)) {
+
+            } else if (mode.matches("(?i)" + Mode.REGISTER)) {
+
+            } else if (mode.matches("(?i)" + Mode.VOTE)) {
+
+            } else if (mode.matches("(?i)" + Mode.EXIT)) {
+                System.out.println("Exiting");
+            } else {
+                System.out.printf("Invalid option: %s%n", mode);
+            }
+        }
+
+
+//        System.out.println("To register, please enter your voter ID and password:");
+//        voterId = input.nextLine();
 
 
 
