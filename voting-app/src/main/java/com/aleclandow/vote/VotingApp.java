e0bd57ae4cfb26123155faae1061250a20f5ea79
Alec Landow <alec.landow@cengage.com>
2021-03-27 17:33:45 -0400

    Add colors

--- voting-app/src/main/java/com/aleclandow/vote/VotingApp.java
+++ voting-app/src/main/java/com/aleclandow/vote/VotingApp.java
@@ -1,4 +1,21 @@
 package com.aleclandow.vote;
 
+import com.aleclandow.util.ConsoleColors;
+import java.util.Scanner;
+
 public class VotingApp {
+    public static void main(String[] args) {
+        System.out.print(ConsoleColors.BLUE);
+        System.out.println("*** Welcome to Alec's Blockchain-Based Voting Program ***");
+        System.out.print(ConsoleColors.RESET);
+
+        String voterId;
+        Scanner input = new Scanner(System.in);
+        System.out.println(ConsoleColors.RED);
+        System.out.println("To register, please enter your voter ID and password:");
+        voterId = input.nextLine();
+
+
+
+    }
 }
